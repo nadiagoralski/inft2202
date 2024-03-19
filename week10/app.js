76f752c205032d1395f05139a763576b5b6b27bb
Nadia Goralski <naadialynn@gmail.com>
2024-03-19 18:23:23 -0400

    dotenv  example and add .gitignore

--- week10/app.js
+++ week10/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const pug = require('pug');
 
+require('dotenv').config();
+
+
+// use port from env or default to 3000 if not set
+const PORT = process.env.PORT || 3000;
 
 // initialize method
 const app = express();
@@ -18,6 +23,8 @@ app.get("/", function(req, res) {
 
 
 
-app.listen(3000, () => {
-    console.log(`Server is listening on port 3000`);
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    // output the value from .env file with the key MY_SECRET_KEY
+    console.log(`My secret value from .env=${process.env.MY_SECRET_KEY}`);
 })
