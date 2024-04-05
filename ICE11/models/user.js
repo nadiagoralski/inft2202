205ea2203734b8a399c3e35c6cf072c0354168eb
Nadia Goralski <naadialynn@gmail.com>
2024-04-04 20:43:25 -0400

    ice11 fixes for user model

--- ICE11/models/user.js
+++ ICE11/models/user.js
@@ -1,22 +1,19 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+mongoose.Promise = global.Promise;
 //  Connect to MongoDB Server using the connection string in the `.env` file.
 const conn = process.env.DB_STRING;
 
-const connection = mongoose.createConnection(conn, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-});
+mongoose.connect(conn);
 
 // Creates simple schema for a User. 
 // The hash and salt are derived from the user's given password when they register
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
 	username: String,
 	hashPassword: String,
 	isAdmin: { type: Boolean, required: true, default: false }
 });
 
-const User = connection.model('User', UserSchema);
 // Expose the connection
-module.exports = connection;
+module.exports.User = mongoose.model('user', userSchema);
